b5fad16b9c7b4b656b762d1388770de5296f8b11
SydneyM <sydneyminnaar2000@hotmail.com>
2021-06-02 17:02:29 +0200

    Update tello_code_execution.py

--- Libraries/tello_code_execution.py
+++ Libraries/tello_code_execution.py
@@ -9,9 +9,9 @@ connection_timeout_in_seconds = None
 try:
     config = json.load(open("config/tello_code_execonfig.json"))
     local_port = int(config["local_port"])
-    tello_drone_ip = int(config["tello_drone_ip"])
+    tello_drone_ip = str(config["tello_drone_ip"])
     tello_drone_port = int(config["tello_drone_port"])
-    connection_timeout_in_seconds = int(config["connection_timeout_in_seconds"])
+    connection_timeout_in_seconds = float(config["connection_timeout_in_seconds"])
 except FileNotFoundError:
     print("No tello-code-execution config file found.")
 
@@ -22,7 +22,7 @@ def send_command(command:str) -> str:
         conn.bind(('', local_port))
         conn.connect((tello_drone_ip, tello_drone_port))
         conn.settimeout(connection_timeout_in_seconds)
-        conn.send(command)
+        conn.send(command.encode('ASCII'))
         resp = conn.recv(1024)
         return resp.decode(errors='replace').strip()
 
